84b8344193fa8bd27f76c1c97c085b2f8cb3a1b9
Rozova <mrozova@imac-fk.harryfox.com>
2016-07-07 15:38:12 -0400

    initial commit

---
+++ application/controllers/site.php
@@ -0,0 +1,24 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Site extends CI_Controller {
+
+	public function index(){
+		$this -> home();
+	}
+
+	public function home(){
+		$this -> load -> view("site_header");
+		$this -> load -> view("site_nav");
+		$this -> load -> view("content_home");
+		$this -> load -> view("site_footer");
+	}
+
+	public function about(){
+		$this -> load -> view("site_header");
+		$this -> load -> view("site_nav");
+		$this -> load -> view("content_about");
+		$this -> load -> view("site_footer");
+	}
+
+}
---
+++ application/views/site_footer.php
@@ -0,0 +1,8 @@
+	<div id="footer">
+		<p>Copyrigt (c) 2016 basicsite.com. All rights reserved.</p>
+	</div>
+
+</div>
+
+</body>
+</html>
